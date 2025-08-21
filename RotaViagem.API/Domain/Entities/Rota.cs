6fc333e4f7a4e3c149729a5279a72c09bcd34347
Alessandro.Oliveira <158335073+LucasI9@users.noreply.github.com>
2025-08-21 16:13:24 -0300

    correcao espaco

--- RotaViagem.API/Domain/Entities/Rota.cs
+++ RotaViagem.API/Domain/Entities/Rota.cs
@@ -9,11 +9,10 @@ namespace Domain.Entities
 {
     public class Rota
     {
-  
         public string id { get; set; }
         public string origem { get; set; }
         public string destino { get; set; }
         public int valor { get; set; }
     }
-   
+
 }
