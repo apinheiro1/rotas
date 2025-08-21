2c9a2c1b76b3af5eed8b90d933648d3f8128d09d
Alessandro.Oliveira <158335073+LucasI9@users.noreply.github.com>
2025-08-21 16:34:57 -0300

    ere

--- RotaViagem.API/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ RotaViagem.API/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30bea503ce16f407e7ee0a3de3a825926dbb3c80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f04617da6006e1c40ad068f9137252fbb4cb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
