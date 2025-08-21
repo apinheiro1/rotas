03205f867782fd9784478f6c951da40f74bcf63f
Alessandro.Oliveira <158335073+LucasI9@users.noreply.github.com>
2025-08-21 15:42:17 -0300

    correcao bd local teste api

--- RotaViagem.API/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ RotaViagem.API/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fdcd68f309c596ebed31005ad7e25cbb911b75b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35426d52c56e022eca46f96015eec7f8a7fa3de6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
